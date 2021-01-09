59563c7f8e2ae1edd66995fe2d00f4afae6261ef
danielturcios <danielturciosa@gmail.com>
2021-01-08 17:24:06 -0800

    basic functionality updated.

--- speech2text.py
+++ speech2text.py
@@ -22,7 +22,7 @@ def callback(recognizer, audio):
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Could not request results from Google Speech Recognition service; {0}.".format(e))
 
 
 def recognize_main():
